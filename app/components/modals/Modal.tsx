ed3246ea33d39a83dcc9a2e38216f1c7edfd7f9b
Hamid Hassani <hamidhassani119@gmail.com>
2023-06-06 18:35:57 +0330

    The handleClose for managing colse modal added to our onClicks

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -60,10 +60,14 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <>
-      <div className='fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-neutral-700/70'>
+      <div
+        onClick={() => handleClose()}
+        className='fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-neutral-700/70'
+      >
         <div className='relative w-full h-full mx-auto my-6 md:w-4/6 lg:w-3/6 xl:w-2/5 lg:h-auto md:h-auto'>
           {/* CONTENT */}
           <div
+            onClick={(e) => e.stopPropagation()}
             className={`
             translate duration-300 h-full
              ${showModal ? "translate-y-0" : "translate-y-full"} 
@@ -85,7 +89,7 @@ const Modal: React.FC<ModalProps> = ({
               </div>
               {/* BODY */}
               <div className='relative flex-auto p-6 '>{body}</div>
-              {/* FOOTER */}
+              {/* FOOTER  */}
               <div className='flex flex-col gap-4 p-6'>
                 <div className='flex flex-row items-center w-full gap-4'>
                   {secondaryAction && secondaryActionLabel && (
